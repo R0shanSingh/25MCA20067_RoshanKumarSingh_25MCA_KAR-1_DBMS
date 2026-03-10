dc022e3fa0d9885bf8943abae9066c48d6d12bd0
Roshan Kumar Singh <94280658+R0shanSingh@users.noreply.github.com>
2026-03-10 12:08:10 +0530

    Create Q2.sql

---
+++ Odd_Q2/Q2.sql
@@ -0,0 +1,24 @@
+-- Problem Statement
+-- A company database contains one tables: Employee(EmpID, EmpName, Salary).
+-- Write an SQL query to create a VIEW named HighSalaryEmployees that displays EmpID, EmpName, and 
+-- Salary from the Employee table where the Salary is greater than 50000.
+
+
+CREATE TABLE Employee (
+    EmpID INT PRIMARY KEY,
+    EmpName VARCHAR(40),
+    Salary INT
+);
+
+INSERT INTO Employee VALUES (1, 'Roshan', 45000);
+INSERT INTO Employee VALUES (2, 'Rittika', 65000);
+INSERT INTO Employee VALUES (3, 'Riya', 70000);
+INSERT INTO Employee VALUES (4, 'Swayam', 50000);
+INSERT INTO Employee VALUES (5, 'Sanchit', 80000);
+
+CREATE VIEW HighSalaryEmployees AS
+SELECT EmpID, EmpName, Salary
+FROM Employee
+WHERE Salary > 50000;
+
+SELECT*FROM HighSalaryEmployees;
